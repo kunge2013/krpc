d95fabcebd85dd355ec5e86a0252d5faabfdcae5
kunge2013 <543976974@qq.com>
2020-05-02 15:41:26 +0800

    1

---
+++ krpc-core/src/main/java/org/kframe/rpc/remoting/RemotingClient.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kframe.rpc.remoting;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import org.kframe.rpc.remoting.exception.RemotingConnectException;
+import org.kframe.rpc.remoting.exception.RemotingSendRequestException;
+import org.kframe.rpc.remoting.exception.RemotingTimeoutException;
+import org.kframe.rpc.remoting.exception.RemotingTooMuchRequestException;
+import org.kframe.rpc.remoting.netty.NettyRequestProcessor;
+import org.kframe.rpc.remoting.protocol.RemotingCommand;
+
+public interface RemotingClient extends RemotingService {
+
+    void updateNameServerAddressList(final List<String> addrs);
+
+    List<String> getNameServerAddressList();
+
+    RemotingCommand invokeSync(final String addr, final RemotingCommand request,
+        final long timeoutMillis) throws InterruptedException, RemotingConnectException,
+        RemotingSendRequestException, RemotingTimeoutException;
+
+    void invokeAsync(final String addr, final RemotingCommand request, final long timeoutMillis,
+        final InvokeCallback invokeCallback) throws InterruptedException, RemotingConnectException,
+        RemotingTooMuchRequestException, RemotingTimeoutException, RemotingSendRequestException;
+
+    void invokeOneway(final String addr, final RemotingCommand request, final long timeoutMillis)
+        throws InterruptedException, RemotingConnectException, RemotingTooMuchRequestException,
+        RemotingTimeoutException, RemotingSendRequestException;
+
+    void registerProcessor(final int requestCode, final NettyRequestProcessor processor,
+        final ExecutorService executor);
+
+    void setCallbackExecutor(final ExecutorService callbackExecutor);
+
+    ExecutorService getCallbackExecutor();
+
+    boolean isChannelWritable(final String addr);
+}
